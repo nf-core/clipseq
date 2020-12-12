d25ba1d88019f7d2047665409192f69a7e823558
Nobby Chakrabarti <anob.chakrabarti@crick.ac.uk>
2020-12-12 10:29:26 +0000

    Updated Rscript to output tables only

--- bin/clip_qc.R
+++ bin/clip_qc.R
@@ -4,34 +4,19 @@
 # A. M. Chakrabarti
 # 10th December 2020
 
-suppressPackageStartupMessages(library(optparse))
-suppressPackageStartupMessages(library(data.table))
-suppressPackageStartupMessages(library(ggplot2))
-suppressPackageStartupMessages(library(scales))
-suppressPackageStartupMessages(library(cowplot))
-suppressPackageStartupMessages(library(ggthemes))
-suppressPackageStartupMessages(library(patchwork))
+suppressPackageStartupMessages(library(reshape2))
 suppressPackageStartupMessages(library(rtracklayer))
 
-option_list <- list(make_option(c("", "--icount"), action = "store_true", type = "logical", default = FALSE, help = "iCount run"),
-                    make_option(c("", "--paraclu"), action = "store_true", type = "logical", default = FALSE, help = "paraclu run"),
-                    make_option(c("", "--pureclip"), action = "store_true", type = "logical", default = FALSE, help = "PureCLIP run"),
-                    make_option(c("", "--piranha"), action = "store_true", type = "logical", default = FALSE, help = "Piranha run"))
+args <- commandArgs(trailingOnly = TRUE)
+args <- c("icount", "paraclu", "pureclip", "piranha")
 
-opt_parser = OptionParser(option_list = option_list)
-opt <- parse_args(opt_parser)
-
-# opt <- list(icount = TRUE,
-#             paraclu = TRUE,
-#             pureclip = TRUE,
-#             piranha = TRUE)
-
+setwd("/Users/chakraa2/projects/clip_pipeline/results_all")
 
 # ==========
 # Functions
 # ==========
 
-load_peaks <- function(peakcaller = "icount") {
+load_peaks <- function(peakcaller) {
   
   if(peakcaller == "xlinks") {
     
@@ -97,9 +82,9 @@ get_metrics <- function(xlinks.list, peaks.list, peakcaller) {
     xlinks_in_peaks <- sum(countOverlaps(peaks.gr, expanded.xlinks.gr))
     xlinksites_in_peaks <- sum(countOverlaps(peaks.gr, xlinks.gr))
     
-    peaks_xlinksite_coverage_percent <- xlinksites_in_peaks/sum(width(peaks.gr))
+    peaks_xlinksite_coverage_percent <- (xlinksites_in_peaks/sum(width(peaks.gr))) * 100
     
-    metrics.dt <- data.table(exp = names(peaks.list)[i],
+    metrics.df <- data.frame(exp = names(peaks.list)[i],
                              peakcaller = peakcaller,
                              total_xlinks = total_xlinks,
                              total_xlinksites = total_xlinksites,
@@ -107,18 +92,19 @@ get_metrics <- function(xlinks.list, peaks.list, peakcaller) {
                              median_peak_width = median_peak_width,
                              mean_peak_width = mean_peak_width,
                              xlinks_in_peaks = xlinks_in_peaks,
-                             xlinks_in_peaks_percent = xlinks_in_peaks/total_xlinks,
+                             xlinks_in_peaks_percent = (xlinks_in_peaks/total_xlinks) * 100,
                              xlinksites_in_peaks = xlinksites_in_peaks,
-                             xlinksites_in_peaks_percent = xlinksites_in_peaks/total_xlinksites,
+                             xlinksites_in_peaks_percent = (xlinksites_in_peaks/total_xlinksites) * 100,
                              peaks_xlinksite_coverage_percent = peaks_xlinksite_coverage_percent)
     
-    return(metrics.dt)
+    return(metrics.df)
     
   })
   
-  metrics.dt <- rbindlist(metrics.list)
+  # metrics.dt <- rbindlist(metrics.list)
+  metrics.df <- do.call(rbind, metrics.list)
   
-  return(metrics.dt)
+  return(metrics.df)
   
 }
 
@@ -132,166 +118,147 @@ get_metrics <- function(xlinks.list, peaks.list, peakcaller) {
 
 xlinks.list <- load_peaks(peakcaller = "xlinks")
 
-xlinks.metrics.dt <- data.table(exp = names(xlinks.list),
+xlinks.metrics.df <- data.frame(exp = names(xlinks.list),
                                 total_xlinks = unlist(lapply(xlinks.list, function(x) sum(x$score))),
                                 total_xlinksites = unlist(lapply(xlinks.list, length)))
 
-xlinks.metrics.dt[, xlink_ratio := total_xlinks/total_xlinksites, by = exp]
+write.table(xlinks.metrics.df, "../plotting/xlinks_metrics.tsv", row.names = FALSE, sep = "\t")
 
-p.total_xlinks <- ggplot(xlinks.metrics.dt, aes(x = exp, y = total_xlinks)) +
-  geom_col(position = "dodge") +
-  scale_fill_tableau() +
-  coord_flip() +
-  scale_y_continuous(labels = comma) +
-  labs(subtitle = "Total crosslinks",
-       x = "Experiment",
-       y = "Number of crosslinks") +
-  theme_minimal_grid() + theme(axis.text.x = element_text(angle = 45, hjust = 1))
-  
-p.total_xlinksites <- ggplot(xlinks.metrics.dt, aes(x = exp, y = total_xlinksites)) +
-  geom_col(position = "dodge") +
-  scale_fill_tableau() +
-  coord_flip() +
-  scale_y_continuous(labels = comma) +
-  labs(subtitle = "Total crosslink sites",
-       x = "",
-       y = "Number of crosslink sites") +
-  theme_minimal_grid() + theme(axis.text.y = element_blank(), axis.text.x = element_text(angle = 45, hjust = 1))
+xlinks.ratio.df <- with(xlinks.metrics.df, data.frame(exp = exp,
+                                                      xlink_ratio = total_xlinks/total_xlinksites))
 
-p.xlink_ratio <- ggplot(xlinks.metrics.dt, aes(x = exp, y = xlink_ratio)) +
-  geom_col(position = "dodge") +
-  scale_fill_tableau() +
-  coord_flip() +
-  scale_y_continuous(labels = comma) +
-  labs(subtitle = "Crosslink ratio",
-       x = "",
-       y = "Crosslink ratio") +
-  theme_minimal_grid() + theme(axis.text.y = element_blank(), axis.text.x = element_text(angle = 45, hjust = 1))
-
-xlinks.patch <- p.total_xlinks + p.total_xlinksites + p.xlink_ratio + plot_annotation(title = "CLIP crosslink metrics")
-
-ggsave(xlinks.patch, filename = "crosslink_metrics.pdf", width = 297, height = 210, units = "mm")
+write.table(xlinks.ratio.df, "../plotting/xlinks_ratio.tsv", row.names = FALSE, sep = "\t")
 
 # ==========
 # Peaks
 # ==========
 
-peakcallers <- names(opt)[unlist(opt)]
+peakcallers <- args
 if(length(peakcallers) == 0) quit(save = "no") # Exit if no peakcallers run
 
 metrics.list <- lapply(peakcallers, function(x) {
   
   peaks.list <- load_peaks(peakcaller = x)
-  peaks.metrics.dt <- get_metrics(xlinks.list = xlinks.list,
+  peaks.metrics.df <- get_metrics(xlinks.list = xlinks.list,
                                   peaks.list = peaks.list,
                                   peakcaller = x)
   
-  return(peaks.metrics.dt)
+  return(peaks.metrics.df)
   
 })
 
-metrics.dt <- rbindlist(metrics.list)
+# metrics.dt <- rbindlist(metrics.list)
+metrics.df <- do.call(rbind, metrics.list)
 
-# Peak calling metrics
+total_peaks.df <- dcast(metrics.df, exp ~ peakcaller, value.var = "total_peaks")
+write.table(total_peaks.df, "../plotting/total_peaks.tsv", row.names = FALSE, sep = "\t")
 
-p.total_peaks <- ggplot(metrics.dt, aes(x = exp, y = total_peaks, fill = peakcaller)) +
-  geom_col(position = "dodge") +
-  scale_fill_tableau() +
-  # facet_grid(. ~ peakcaller) +
-  coord_flip() +
-  scale_y_continuous(labels = comma) +
-  labs(subtitle = "Total peaks",
-       x = "Experiment",
-       y = "Count",
-       fill = "Peak caller") +
-  theme_minimal_grid() + theme(axis.text.x = element_text(angle = 45, hjust = 1))
+xlinks_in_peaks_percent.df <- dcast(metrics.df, exp ~ peakcaller, value.var = "xlinks_in_peaks_percent")
+write.table(xlinks_in_peaks_percent.df, "../plotting/xlinks_in_peaks.tsv", row.names = FALSE, sep = "\t")
 
-p.xlinks_in_peaks <- ggplot(metrics.dt, aes(x = exp, y = xlinks_in_peaks_percent, fill = peakcaller)) +
-  geom_col(position = "dodge") +
-  scale_fill_tableau() +
-  # facet_grid(. ~ peakcaller) +
-  coord_flip() +
-  scale_y_continuous(labels = comma) +
-  labs(subtitle = "Crosslinks within peaks",
-       x = "",
-       y = "Fraction",
-       fill = "Peak caller") +
-  theme_minimal_grid() + theme(axis.text.y = element_blank(), axis.text.x = element_text(angle = 45, hjust = 1))
+xlinksites_in_peaks_percent.df <- dcast(metrics.df, exp ~ peakcaller, value.var = "xlinksites_in_peaks_percent")
+write.table(xlinksites_in_peaks_percent.df, "../plotting/xlinksites_in_peaks.tsv", row.names = FALSE, sep = "\t")
 
-p.xlinksites_in_peaks <- ggplot(metrics.dt, aes(x = exp, y = xlinksites_in_peaks_percent, fill = peakcaller)) +
-  geom_col(position = "dodge") +
-  scale_fill_tableau() +
-  # facet_grid(. ~ peakcaller) +
-  coord_flip() +
-  labs(subtitle = "Crosslink sites within peaks",
-       x = "",
-       y = "Fraction",
-       fill = "Peak caller") +
-  theme_minimal_grid() + theme(axis.text.y = element_blank(), axis.text.x = element_text(angle = 45, hjust = 1))
+peaks_xlinksite_coverage_percent.df <- dcast(metrics.df, exp ~ peakcaller, value.var = "peaks_xlinksite_coverage_percent")
+write.table(peaks_xlinksite_coverage_percent.df, "../plotting/peaks_xlinksite_coverage.tsv", row.names = FALSE, sep = "\t")
 
-peaks1.patch <- p.total_peaks + p.xlinks_in_peaks + p.xlinksites_in_peaks +
-  plot_layout(guides = "collect") + plot_annotation(title = "CLIP peak metrics")
-
-ggsave(peaks1.patch, filename = "peak_metrics_1.pdf", width = 297, height = 210, units = "mm")
-
-p.peaks_xlinksite_coverage <- ggplot(metrics.dt, aes(x = exp, y = peaks_xlinksite_coverage_percent, fill = peakcaller)) +
-  geom_col(position = "dodge") +
-  scale_fill_tableau() +
-  # facet_grid(. ~ peakcaller) +
-  coord_flip() +
-  labs(subtitle = "Coverage of peaks with crosslink sites",
-       x = "",
-       y = "Fraction",
-       fill = "Peak caller") +
-  theme_minimal_grid() + theme(axis.text.y = element_blank(), axis.text.x = element_text(angle = 45, hjust = 1))
-
-peaks2.patch <- p.total_peaks + p.xlinks_in_peaks + p.xlinksites_in_peaks +
-  plot_layout(guides = "collect") + plot_annotation(title = "CLIP peak metrics")
-
-ggsave(peaks2.patch, filename = "peak_metrics_2.pdf", width = 297, height = 210, units = "mm")
-
-# Peak widths
-
-p.median_peak_width <- ggplot(metrics.dt, aes(x = exp, y = median_peak_width, fill = peakcaller)) +
-  geom_col(position = "dodge") +
-  scale_fill_tableau() +
-  # facet_grid(. ~ peakcaller) +
-  coord_flip() +
-  scale_y_continuous(labels = comma) +
-  labs(title = "",
-       x = "Experiment",
-       y = "Median peak width",
-       fill = "Peak caller") +
-  theme_minimal_grid() + theme(axis.text.x = element_text(angle = 45, hjust = 1))
-
-p.mean_peak_width <- ggplot(metrics.dt, aes(x = exp, y = mean_peak_width, fill = peakcaller)) +
-  geom_col(position = "dodge") +
-  scale_fill_tableau() +
-  # facet_grid(. ~ peakcaller) +
-  coord_flip() +
-  scale_y_continuous(labels = comma) +
-  labs(title = "",
-       x = "",
-       y = "Mean peak width",
-       fill = "Peak caller") +
-  theme_minimal_grid() + theme(axis.text.y = element_blank(), axis.text.x = element_text(angle = 45, hjust = 1))
-
-peakwidths.patch <- p.median_peak_width + p.mean_peak_width + 
-  plot_layout(guides = "collect") + plot_annotation(title = "CLIP peak widths")
-
-ggsave(peakwidths.patch, filename = "peak_metrics_3.pdf", width = 297, height = 210, units = "mm")
-
-# # peak widths
-# icount.peaks.width.dt <- data.table(exp = rep(names(icount.list), elementNROWS(icount.list)),
-#                                     widths = unlist(lapply(icount.list, width)))
+# # Peak calling metrics
 # 
-# ggplot(icount.peaks.width.dt, aes(x = exp, y = widths, colour = exp)) +
-#   geom_boxplot() + 
-#   coord_flip() + 
-#   labs(title = "iCount peak width",
+# p.total_peaks <- ggplot(metrics.dt, aes(x = exp, y = total_peaks, fill = peakcaller)) +
+#   geom_col(position = "dodge") +
+#   scale_fill_tableau() +
+#   # facet_grid(. ~ peakcaller) +
+#   coord_flip() +
+#   scale_y_continuous(labels = comma) +
+#   labs(subtitle = "Total peaks",
 #        x = "Experiment",
-#        y = "Peak width") +
-#   theme_minimal_grid() + theme(legend.position = "none")
-
-
-
+#        y = "Count",
+#        fill = "Peak caller") +
+#   theme_minimal_grid() + theme(axis.text.x = element_text(angle = 45, hjust = 1))
+# 
+# p.xlinks_in_peaks <- ggplot(metrics.dt, aes(x = exp, y = xlinks_in_peaks_percent, fill = peakcaller)) +
+#   geom_col(position = "dodge") +
+#   scale_fill_tableau() +
+#   # facet_grid(. ~ peakcaller) +
+#   coord_flip() +
+#   scale_y_continuous(labels = comma) +
+#   labs(subtitle = "Crosslinks within peaks",
+#        x = "",
+#        y = "Fraction",
+#        fill = "Peak caller") +
+#   theme_minimal_grid() + theme(axis.text.y = element_blank(), axis.text.x = element_text(angle = 45, hjust = 1))
+# 
+# p.xlinksites_in_peaks <- ggplot(metrics.dt, aes(x = exp, y = xlinksites_in_peaks_percent, fill = peakcaller)) +
+#   geom_col(position = "dodge") +
+#   scale_fill_tableau() +
+#   # facet_grid(. ~ peakcaller) +
+#   coord_flip() +
+#   labs(subtitle = "Crosslink sites within peaks",
+#        x = "",
+#        y = "Fraction",
+#        fill = "Peak caller") +
+#   theme_minimal_grid() + theme(axis.text.y = element_blank(), axis.text.x = element_text(angle = 45, hjust = 1))
+# 
+# peaks1.patch <- p.total_peaks + p.xlinks_in_peaks + p.xlinksites_in_peaks +
+#   plot_layout(guides = "collect") + plot_annotation(title = "CLIP peak metrics")
+# 
+# ggsave(peaks1.patch, filename = "peak_metrics_1.pdf", width = 297, height = 210, units = "mm")
+# 
+# p.peaks_xlinksite_coverage <- ggplot(metrics.dt, aes(x = exp, y = peaks_xlinksite_coverage_percent, fill = peakcaller)) +
+#   geom_col(position = "dodge") +
+#   scale_fill_tableau() +
+#   # facet_grid(. ~ peakcaller) +
+#   coord_flip() +
+#   labs(subtitle = "Coverage of peaks with crosslink sites",
+#        x = "",
+#        y = "Fraction",
+#        fill = "Peak caller") +
+#   theme_minimal_grid() + theme(axis.text.y = element_blank(), axis.text.x = element_text(angle = 45, hjust = 1))
+# 
+# peaks2.patch <- p.total_peaks + p.xlinks_in_peaks + p.xlinksites_in_peaks +
+#   plot_layout(guides = "collect") + plot_annotation(title = "CLIP peak metrics")
+# 
+# ggsave(peaks2.patch, filename = "peak_metrics_2.pdf", width = 297, height = 210, units = "mm")
+# 
+# # Peak widths
+# 
+# p.median_peak_width <- ggplot(metrics.dt, aes(x = exp, y = median_peak_width, fill = peakcaller)) +
+#   geom_col(position = "dodge") +
+#   scale_fill_tableau() +
+#   # facet_grid(. ~ peakcaller) +
+#   coord_flip() +
+#   scale_y_continuous(labels = comma) +
+#   labs(title = "",
+#        x = "Experiment",
+#        y = "Median peak width",
+#        fill = "Peak caller") +
+#   theme_minimal_grid() + theme(axis.text.x = element_text(angle = 45, hjust = 1))
+# 
+# p.mean_peak_width <- ggplot(metrics.dt, aes(x = exp, y = mean_peak_width, fill = peakcaller)) +
+#   geom_col(position = "dodge") +
+#   scale_fill_tableau() +
+#   # facet_grid(. ~ peakcaller) +
+#   coord_flip() +
+#   scale_y_continuous(labels = comma) +
+#   labs(title = "",
+#        x = "",
+#        y = "Mean peak width",
+#        fill = "Peak caller") +
+#   theme_minimal_grid() + theme(axis.text.y = element_blank(), axis.text.x = element_text(angle = 45, hjust = 1))
+# 
+# peakwidths.patch <- p.median_peak_width + p.mean_peak_width + 
+#   plot_layout(guides = "collect") + plot_annotation(title = "CLIP peak widths")
+# 
+# ggsave(peakwidths.patch, filename = "peak_metrics_3.pdf", width = 297, height = 210, units = "mm")
+# 
+# # # peak widths
+# # icount.peaks.width.dt <- data.table(exp = rep(names(icount.list), elementNROWS(icount.list)),
+# #                                     widths = unlist(lapply(icount.list, width)))
+# # 
+# # ggplot(icount.peaks.width.dt, aes(x = exp, y = widths, colour = exp)) +
+# #   geom_boxplot() + 
+# #   coord_flip() + 
+# #   labs(title = "iCount peak width",
+# #        x = "Experiment",
+# #        y = "Peak width") +
+# #   theme_minimal_grid() + theme(legend.position = "none")
